8e439fd06bd9f5332333cb2f145a3c432062a3a1
Henok Birru <henokbirru@dyn138-84.eduroam.utu.fi>
2023-02-02 15:02:48 +0200

    Form interface added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import List from './components/List';
 import './App.css';
 
+interface IState {
+  people: {
+    name: string,
+    age: number,
+    url: string,
+    note?: string 
+  }[]
+}
+
+
 function App() {
+  const [numbers, setNumber] = useState(5);
+  const [people, setPeople] = useState<IState["people"]>([
+    {name: 'Abebe', age: 20, url: 'test'},
+  ]);
+
+
+  const onSubmit = () => {
+
+  }
+
+  const onChange = () => {
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='parent-div'>
+      <div className="card">
+        <form onSubmit={onSubmit}>
+          <div className="form-item">
+            <label htmlFor="Cart Value" className='form-label'>Cart Value</label>
+            <input type="number" placeholder='Enter Cart Value' onChange={onChange} className="form-control" required/>
+          </div>
+          <div className="form-item">
+            <label htmlFor="Delivery Distance" className='form-label'>Delivery Distance</label>
+            <input type="number" placeholder="Enter Delivery Distance" onChange={onChange} className="form-control" required/>
+          </div>
+          <div className="form-item">
+            <label htmlFor="Amount of Items" className='form-label'>Amount of Items</label>
+            <input type="number" placeholder="Enter Amount of Items" onChange={onChange} className="form-control" required/>
+          </div>
+          <div className="form-item">
+            <label htmlFor="Order Time" className='form-label'>Order Time</label>
+            <input type="date" onChange={onChange} className="form-control" required/>
+          </div>
+          <div className="form-item">
+            <button className='btn'>Calculate Delivery Price</button>
+          </div>
+        </form>
+        <p className="form-item">Delivery Price: 2€</p>
+      </div>
     </div>
   );
 }
