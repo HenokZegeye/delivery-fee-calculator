22002f1eb81b8c6f321be93f1881c389286d66bf
Henok Birru <henokbirru@dyn60-27.yok.fi>
2023-02-03 00:04:32 +0200

    useReducer hook added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import List from './components/List';
+import React, { useState, useReducer } from 'react';
 import './App.css';
 
 interface IState {
@@ -11,47 +10,50 @@ interface IState {
   }[]
 }
 
+const formReducer = (state: any, {target}: any) => {
+  return {
+    ...state,
+    [target.name]: target.value
+  }
+}
 
 function App() {
-  const [numbers, setNumber] = useState(5);
-  const [people, setPeople] = useState<IState["people"]>([
-    {name: 'Abebe', age: 20, url: 'test'},
-  ]);
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [deliveryPrice, setDeliveryPrice] = useState(2);
 
-
-  const onSubmit = () => {
-
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(formData);
+    
   }
 
-  const onChange = () => {
-
-  }
   return (
     <div className='parent-div'>
       <div className="card">
         <form onSubmit={onSubmit}>
           <div className="form-item">
             <label htmlFor="Cart Value" className='form-label'>Cart Value</label>
-            <input type="number" placeholder='Enter Cart Value' onChange={onChange} className="form-control" required/>
+            <input type="number" name='cart_value' placeholder='Enter Cart Value' onChange={setFormData} className="form-control" required/>
           </div>
           <div className="form-item">
             <label htmlFor="Delivery Distance" className='form-label'>Delivery Distance</label>
-            <input type="number" placeholder="Enter Delivery Distance" onChange={onChange} className="form-control" required/>
+            <input type="number" name='delivery_distance' placeholder="Enter Delivery Distance" onChange={setFormData} className="form-control" required/>
           </div>
           <div className="form-item">
             <label htmlFor="Amount of Items" className='form-label'>Amount of Items</label>
-            <input type="number" placeholder="Enter Amount of Items" onChange={onChange} className="form-control" required/>
+            <input type="number" name='amount_of_items' placeholder="Enter Amount of Items" onChange={setFormData} className="form-control" required/>
           </div>
           <div className="form-item">
             <label htmlFor="Order Time" className='form-label'>Order Time</label>
-            <input type="date" onChange={onChange} className="form-control" required/>
+            <input type="datetime" name='date' onChange={setFormData} className="form-control" required/>
           </div>
           <div className="form-item">
-            <button className='btn'>Calculate Delivery Price</button>
+            <button type='submit' className='btn'>Calculate Delivery Price</button>
           </div>
         </form>
-        <p className="form-item">Delivery Price: 2€</p>
+        <p className="form-item">Delivery Price: {deliveryPrice}€</p>
       </div>
+      
     </div>
   );
 }
