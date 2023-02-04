cc3e24a118200605cd3e86577e931b2f176c3093
Henok Birru <henokbirru@dyn60-27.yok.fi>
2023-02-04 23:51:34 +0200

    Logo display tested

--- src/components/ui/Logo.test.tsx
+++ src/components/ui/Logo.test.tsx
@@ -1,11 +1,15 @@
-import { render } from "@testing-library/react";
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import _ from "react";
 import Logo from "./Logo";
+
+Enzyme.configure({adapter: new Adapter()});
 
 const logopath = process.env.PUBLIC_URL + '/wolt-logo.jpeg';
 
 describe('<Logo/>', () => {
     it('renders Wolt logo', () => {
-        const logo = render(<Logo path={logopath}/>);
+        const logo = shallow(<Logo path={logopath}/>);
+        expect(logo.find("img").prop("src")).toEqual(logopath);
     });
 });
